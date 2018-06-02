45a007245608134f5704e6283a5e927dd19a13c5
alocke12992 <alocke12992@gmail.com>
2018-06-01 18:43:34 -0600

    Adds home component

--- src/root-application/root-application.js
+++ src/root-application/root-application.js
@@ -2,6 +2,7 @@ import {registerApplication, start} from 'single-spa';
 
 registerApplication('navBar', () => import ('../navBar/navBar.js'), () => true);
 registerApplication('reactApp', () => import ('../reactApp/reactApp.js'), pathPrefix('/reactApp'));
+registerApplication('home', () => import('../home/home.js'), () => location.pathname === "" || location.pathname === "/");
 
 
 start();
